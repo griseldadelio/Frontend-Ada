f882fbb2cd663dc3858ef9dca566e9f9218e1a70
griselda de lio <griseldadelio@hotmail.com>
2020-12-02 12:06:02 -0300

    consignas terminadas

--- clase-66/clase66.js
+++ clase-66/clase66.js
@@ -361,19 +361,16 @@ console.log(retirarDinero(10000))
 console.log(retirarDinero(30000))
 */
 
-/*
-Crear un programa que simule un Tamagochi (mascota virtual). Debe tener 4 variables: salud, felicidad, limpieza, energía, cuyos
-//valores pueden ir entre 1 y 10. El programa debe iniciar mostrando un mensaje con el nombre del tamagochi y los valores de cada
-//variable (que son asignados aleatoriamente). Luego, debe mostrar las siguientes posibles acciones a realizar, que modifican los
-//valores de la siguiente forma:
-ALIMENTAR: +3 energía, +2 felicidad
-JUGAR: +2 felicidad, -2 energía, -1 limpieza
-DORMIR: +5 energía, +2 salud, -2 limpieza
-VACUNAR: +5 salud, -6 felicidad
-BAÑAR: +3 salud
-RETAR: -3 felicidad
-ACARICIAR: +4 felicidad
-Cuando el usuario selecciona una, debe mostrar el valor final de las variables.
+
+//Crear un programa que simule un Tamagochi (mascota virtual). Debe tener 4 variables: salud, felicidad, limpieza, energía, cuyos valores pueden ir entre 1 y 10. El programa debe iniciar mostrando un mensaje con el nombre del tamagochi y los valores de cada variable (que son asignados aleatoriamente). Luego, debe mostrar las siguientes posibles acciones a realizar, que modifican los valores de la siguiente forma:
+//ALIMENTAR: +3 energía, +2 felicidad
+//JUGAR: +2 felicidad, -2 energía, -1 limpieza
+//DORMIR: +5 energía, +2 salud, -2 limpieza
+//VACUNAR: +5 salud, -6 felicidad
+//BAÑAR: +3 salud
+//RETAR: -3 felicidad
+//ACARICIAR: +4 felicidad
+//Cuando el usuario selecciona una, debe mostrar el valor final de las variables.
 const getRandom = () => {
     return Math.ceil(Math.random() * 10);
 };
@@ -461,4 +458,3 @@ const tamagochi = (action) => {
     tamagochiMenu(action);
     updateSpan();
 };
-*/
