5371aaa4f689b49c6be1838f415cae4e12c71b25
Griselda <65302127+griseldadelio@users.noreply.github.com>
2021-01-06 11:21:36 -0300

    Delete scripts.js

--- ejercicios/users/scripts.js
+++
@@ -1,8 +0,0 @@
-const urlBase = 'https://ada-api-clase86-default-rtdb.firebaseio.com';
-
-const creatObject = () => {
-    const user = document.getElementById('user').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    return { user, email, password }
-}
