6a9ebd27e1effa0d3ba8a58953aabd7f944559cf
Griselda <65302127+griseldadelio@users.noreply.github.com>
2021-01-06 11:20:36 -0300

    Delete scripts.js

--- ejercicios/pets/scripts.js
+++
@@ -1,9 +0,0 @@
-const urlBase = 'https://ada-api-clase86-default-rtdb.firebaseio.com';
-
-const creatObject = () => {
-    const name = document.getElementById('name').value;
-    const breed = document.getElementById('breed').value;
-    const weight = document.getElementById('weight').value;
-    const age = document.getElementById('age').value;
-    return { name, breed, weight, age }
-}
